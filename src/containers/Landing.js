998fd65ef226a52e926372b4d184152c9d5e5013
Ian Logan <itlogan9@gmail.com>
2019-10-09 18:12:54 -0500

    Render landing page

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -1,10 +1,21 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 function Landing() {
   return (
-    <div>
-      <h1>Landing</h1>
-    </div>
+    <header>
+      <div className="flex justify-center">
+        <div className="max-w-xs">
+          <h1 className="text-4xl ">Simplegraphs</h1>
+          <div className="flex justify-center">
+            <Link to="/login">
+              <button className="mt-4 btn btn-blue">Login</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 }
 
