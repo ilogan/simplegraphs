c1f7548829803cc954e53542c5707f6d2386f2e2
Ian Logan <itlogan9@gmail.com>
2019-10-09 18:02:55 -0500

    Render login page

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,9 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Login() {
+function Login({ history }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const isValidated = () => {
+    return username.length > 0 && password.length > 0;
+  };
+
+  const validationStyles = () => {
+    return isValidated()
+      ? "btn btn-blue"
+      : "btn btn-blue bg-blue-200 hover:bg-blue-200 cursor-not-allowed";
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      alert("Success");
+      history.push("/");
+    } catch (e) {
+      alert("Login error:", e.message);
+    }
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="flex justify-center">
+      <form className="w-full max-w-xs md:max-w-sm" onSubmit={handleSubmit}>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label
+              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+              htmlFor="username"
+            >
+              Username
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+              id="username"
+              type="text"
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label
+              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+              htmlFor="password"
+            >
+              Password
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+              id="password"
+              type="password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/3"></div>
+          <div className="md:w-2/3">
+            <button className={validationStyles()} disabled={!isValidated()}>
+              Login
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
