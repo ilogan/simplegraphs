388c6b4586720ed3483a8786385044558761a8be
Ian Logan <itlogan9@gmail.com>
2019-10-09 18:52:29 -0500

    Add route for 404 errors

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,14 @@ import { Route, Switch } from "react-router-dom";
 
 import Landing from "./containers/Landing";
 import Login from "./containers/Login";
+import NotFound from "./components/NotFound";
 
 export default () => {
   return (
     <Switch>
       <Route path="/" exact component={Landing} />
       <Route path="/login" exact component={Login} />
+      <Route component={NotFound} />
     </Switch>
   );
 };
---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default () => (
+  <div className="flex justify-center">
+    <div className="max-w-sm px-4">
+      <h3>
+        <span className="font-bold">Sorry!</span> We couldn't find the page you
+        were looking for.
+      </h3>
+    </div>
+  </div>
+);
