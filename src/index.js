5f664dc5f35cc8cadc72f0734d71dea92c4545a8
Ian Logan <itlogan9@gmail.com>
2019-10-09 17:01:13 -0500

    Setup tailwind

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import "./styles/index.css";
 require("dotenv").config();
 
 ReactDOM.render(<App />, document.getElementById("root"));
