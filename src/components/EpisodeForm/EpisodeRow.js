7a3925c2e3539414391305cb41183480864d0fd1
Ian Logan <itlogan9@gmail.com>
2019-10-09 22:12:32 -0500

    Style episode rows

--- src/components/EpisodeForm/EpisodeRow.js
+++ src/components/EpisodeForm/EpisodeRow.js
@@ -4,11 +4,10 @@ function EpisodeRow({ episode, id, updateEpisode, renderShowAllText }) {
   const toggleGraphShow = () => {
     updateEpisode(id, "showOnGraph", !episode.showOnGraph);
   };
-
   const publishDate = new Date(episode.published_at);
   return (
-    <tr>
-      <td className="">{episode.title}</td>
+    <tr className={episode.showOnGraph ? "" : "text-gray-500"}>
+      <td>{episode.title}</td>
       <td>{episode.number}</td>
       <td>{publishDate.toDateString()}</td>
       <td className="text-center">
@@ -21,18 +20,13 @@ function EpisodeRow({ episode, id, updateEpisode, renderShowAllText }) {
           </button>
         ) : (
           <button
-            className="btn btn-green px-1 py-1 text-sm"
+            className="btn border border-blue-400 text-blue-400 px-1 py-1 text-sm"
             onClick={toggleGraphShow}
           >
             Add
           </button>
         )}
       </td>
-      {episode.showOnGraph ? (
-        <td className="pl-4 text-green-600">O</td>
-      ) : (
-        <td className="pl-4 text-red-500">X</td>
-      )}
     </tr>
   );
 }
