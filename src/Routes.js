9837ff8241ee128d7e6006b5e01e4c15251b5d81
Ian Logan <itlogan9@gmail.com>
2019-10-09 19:09:46 -0500

    Create applied route to pass props

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import AppliedRoute from "./components/AppliedRoute";
 
 import Landing from "./containers/Landing";
 import Login from "./containers/Login";
 import NotFound from "./components/NotFound";
 
-export default () => {
+export default props => {
   return (
     <Switch>
-      <Route path="/" exact component={Landing} />
-      <Route path="/login" exact component={Login} />
+      <AppliedRoute path="/" props={props} exact component={Landing} />
+      <AppliedRoute path="/login" props={props} exact component={Login} />
       <Route component={NotFound} />
     </Switch>
   );
