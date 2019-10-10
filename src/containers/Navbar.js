2f28f72bd1f35adf73b9a9b3d0685d29882311f1
Ian Logan <itlogan9@gmail.com>
2019-10-09 19:34:43 -0500

    Add logout functionality

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -1,8 +1,27 @@
 import React from "react";
+import { Auth } from "aws-amplify";
 
 import { Link } from "react-router-dom";
 
-function Navbar() {
+function Navbar({ authState }) {
+  // remove user credentials after hitting logout button
+  const handleLogout = async () => {
+    await Auth.signOut();
+  };
+
+  const logStyle = "inline-block text-orange-200 hover:text-white mr-4";
+  const renderLogin = () => (
+    <Link to="/login" className={logStyle}>
+      Login
+    </Link>
+  );
+
+  const renderLogout = () => (
+    <Link to="/login" className={logStyle} onClick={handleLogout}>
+      Logout
+    </Link>
+  );
+
   return (
     <nav className="flex items-center justify-between flex-wrap bg-orange-500 p-6 mb-8">
       <div className="flex items-center text-white mr-6">
@@ -15,12 +34,7 @@ function Navbar() {
 
       <div className="block flex items-center">
         <div className="text-sm">
-          <Link
-            to="/login"
-            className="inline-block text-orange-200 hover:text-white mr-4"
-          >
-            Login
-          </Link>
+          {authState === "signedIn" ? renderLogout() : renderLogin()}
         </div>
       </div>
     </nav>
