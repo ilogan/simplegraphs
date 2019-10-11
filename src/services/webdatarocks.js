d293b2239706dbc91162ba247d0f586bc1a51749
Ian Logan <itlogan9@gmail.com>
2019-10-11 11:40:25 -0500

    Customize how webdatarocks data is transformed

--- src/services/webdatarocks.js
+++ src/services/webdatarocks.js
@@ -1,12 +1,21 @@
-//
+// takes list of downloads data from  /analytics/downloads?episode={episodeId}
+// converts into format consumable by webdatarocks
 const importDownloadsData = (episodeDownloadList, episodeList) => {
   let data = [];
   episodeDownloadList.forEach(episodeDownloads => {
     // match the ids to get an episode containing a title
     const episode = episodeList.find(ep => ep.id === episodeDownloads.id);
-    episodeDownloads.by_interval.forEach(day => {
+    episodeDownloads.by_interval.forEach((day, i) => {
       const dayData = { ...day };
-      dayData["title"] = episode.title;
+      dayData["Title"] = episode.title;
+
+      dayData["Downloads"] = day.downloads_total;
+      delete dayData.downloads_total;
+
+      dayData["Download Percentage"] = day.downloads_percent;
+      delete dayData.downloads_percent;
+
+      dayData["Days Since Release"] = i + 1;
       data.push(dayData);
     });
   });
