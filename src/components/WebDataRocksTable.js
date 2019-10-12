8792bf7e7b51619fdb15107c1a1ce774002b3b24
Ian Logan <itlogan9@gmail.com>
2019-10-11 23:06:43 -0500

    Add WebDataRocks instructions

--- src/components/WebDataRocksTable.js
+++ src/components/WebDataRocksTable.js
@@ -15,6 +15,30 @@ function WebDataRocksTable({ report }) {
   return (
     <div className="mt-20 w-full">
       <div id="pivot-table-container"></div>
+      <div className="text-sm">
+        <p className="mt-10">
+          This table initializes showing the number of downloads for the first
+          30 days of a podcast episode's life. You can remove the 30 days filter
+          by first clicking "DAYS SINCE RELEASE" then clicking "Select All". The
+          pivot table can be further customized by clicking on fields and moving
+          data around as you see fit!
+        </p>
+        <p className="mt-4">
+          Now, I know what you're thinking... "Simplegraphs? More like
+          ComplicatedPivotTable!" Look, there was a graph, but it didn't look
+          that great. More graphs to come in the future. All in good time! Stay
+          tuned for updates on my{" "}
+          <a
+            href="https://github.com/ilogan/simplegraphs"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link"
+          >
+            github
+          </a>
+          !
+        </p>
+      </div>
     </div>
   );
 }
