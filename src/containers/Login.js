c3e73dd86d317c4dd7a2358bcd6ecf28ef8864d0
Ian Logan <itlogan9@gmail.com>
2019-10-11 21:48:35 -0500

    Add login credentials

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -57,55 +57,80 @@ function Login({ history, cProps }) {
   };
 
   return (
-    <div className="flex justify-center">
-      <form className="w-full max-w-xs md:max-w-sm" onSubmit={handleSubmit}>
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
-            <label
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              htmlFor="username"
-            >
-              Username
-            </label>
+    <div className="top-container">
+      <div className="center-container">
+        <form className="w-full max-w-xs md:max-w-sm" onSubmit={handleSubmit}>
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label
+                className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                htmlFor="username"
+              >
+                Username
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+                id="username"
+                type="text"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+              />
+            </div>
           </div>
-          <div className="md:w-2/3">
-            <input
-              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-              id="username"
-              type="text"
-              value={username}
-              onChange={e => setUsername(e.target.value)}
-            />
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label
+                className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                htmlFor="password"
+              >
+                Password
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+                id="password"
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="md:flex md:items-center">
+            <div className="md:w-1/3"></div>
+            <div className="md:w-2/3">
+              <button className={validationStyles()} disabled={!isValidated()}>
+                Login
+              </button>
+            </div>
+          </div>
+        </form>
+
+        <div className="mt-10 text-sm">
+          <div className="md:flex md:items-center">
+            <p className="">
+              Simplecast requires credentials to access their API. Only an admin
+              can sign up users for the time being, so I created a demo account
+              for demonstraion purposes.
+            </p>
+          </div>
+
+          <div className="mt-6 md:flex md:items-center">
+            <div className="flex">
+              <p>
+                Username: <br />
+                <span className="font-semibold">demo-user</span>
+              </p>
+              <p className="ml-8">
+                Password: <br />
+                <span className="font-semibold">Realpassword1!</span>
+              </p>
+            </div>
           </div>
         </div>
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
-            <label
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              htmlFor="password"
-            >
-              Password
-            </label>
-          </div>
-          <div className="md:w-2/3">
-            <input
-              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-              id="password"
-              type="password"
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-            />
-          </div>
-        </div>
-        <div className="md:flex md:items-center">
-          <div className="md:w-1/3"></div>
-          <div className="md:w-2/3">
-            <button className={validationStyles()} disabled={!isValidated()}>
-              Login
-            </button>
-          </div>
-        </div>
-      </form>
+      </div>
     </div>
   );
 }
